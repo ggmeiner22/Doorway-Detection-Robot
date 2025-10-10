7a7021928e0e636566537cd031a72b2bf35024e7
javo5555 <jlaboyjusino2021@my.fit.edu>
2025-10-10 13:51:08 -0400

    FIxed connection issues with the robot.

--- src/scripts/collect.py
+++ src/scripts/collect.py
@@ -1,14 +1,22 @@
-from __future__ import annotations
-import asyncio
+import time, threading
+from irobot_edu_sdk.robots import event
+from irobot_edu_sdk.music import Note
 from irobot_edu_sdk.backend.bluetooth import Bluetooth
 from irobot_edu_sdk.robots import Create3
-from src.core.control import collect_data_manual
-from src.core.config import DATA_CSV, ROBOT_NAME
+from src.core.config import CPTS_DIR, DT, ROBOT_NAME, ensure_dirs, DOOR_STATES, DATA_CSV
+from src.core.control import run_controller,collect_data_manual
 
+robot = Create3(Bluetooth(ROBOT_NAME))
+print("[run] Using event-driven SDK loop")
 
-async def main():
-    robot = Create3(Bluetooth(ROBOT_NAME))
+@event(robot.when_play)
+async def play(robot):
+    await robot.set_lights_on_rgb(30, 255, 100)
+    await robot.play_note(Note.A5, .5)
+    print(f"[run] Connected to {ROBOT_NAME}")
+    ensure_dirs()
+
+    #Collect Data
     await collect_data_manual(robot, data_csv=DATA_CSV)
 
-if __name__ == "__main__":
-    asyncio.run(main())
+robot.play()
