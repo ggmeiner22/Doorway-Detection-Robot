ec7a927d6cd34409b7a7d0924bd5d5edcb7dac32
Garrett <ggmeiner22@gmail.com>
2025-10-11 09:44:36 -0400

    Added collision backoff

---
+++ src/core/motion.py
@@ -0,0 +1,20 @@
+from .config import FORWARD
+
+async def forward(robot):
+    await robot.set_wheel_speeds(FORWARD, FORWARD)
+
+async def backoff_left(robot):
+    await robot.set_lights_on_rgb(255, 80, 0)
+    await robot.move(-10)           # units: confirm cm vs mm
+    await robot.turn_left(20)
+    #await robot.forward
+
+async def backoff_right(robot):
+    await robot.set_lights_on_rgb(255, 80, 0)
+    await robot.move(-10)
+    await robot.turn_left(20)      # Test with turn_right vs turn_left
+    #await robot.forward
+    
+def front_obstacle_cm(sensors, approx):
+    """Return min front distance in cm using your approximate_distance() callable."""
+    return min(approx(sensors[3]), approx(sensors[4]))
--- src/scripts/collect.py
+++ src/scripts/collect.py
@@ -1,13 +1,26 @@
-import time, threading
 from irobot_edu_sdk.robots import event
 from irobot_edu_sdk.music import Note
 from irobot_edu_sdk.backend.bluetooth import Bluetooth
 from irobot_edu_sdk.robots import Create3
 from src.core.config import CPTS_DIR, DT, ROBOT_NAME, ensure_dirs, DOOR_STATES, DATA_CSV
-from src.core.control import run_controller,collect_data_manual
+from src.core.control import collect_data_manual
+from src.core.motion import backoff_right, backoff_left
 
-robot = Create3(Bluetooth(ROBOT_NAME))
+robot = Create3(Bluetooth())
 print("[run] Using event-driven SDK loop")
+
+# Register event handlers ***after*** robot is created
+@event(robot.when_bumped, [False, True])   # right bumper
+async def avoidcollision_right(robot_event):
+    print("Right bumper: backoff")
+    await backoff_right(robot)
+    await robot.set_lights_on_rgb(30, 255, 100)
+
+@event(robot.when_bumped, [True, True])    # left bumper
+async def avoidcollision_left(robot_event):
+    print("Left bumper: backoff")
+    await backoff_left(robot)
+    await robot.set_lights_on_rgb(30, 255, 100)
 
 @event(robot.when_play)
 async def play(robot):
